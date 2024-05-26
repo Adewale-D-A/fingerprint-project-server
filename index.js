f611f9a18ff0929c95c6996563fc49e0de2d23f4
adewale-d-a <adewale-d-a.com>
2024-05-26 12:51:40 +0100

    CORS domain listings

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+// app.use(cors());
+app.use(
+  cors({
+    origin: ["*", process.env.LOCALHOST, process.env.FRONTEND_DOMAIN],
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 
